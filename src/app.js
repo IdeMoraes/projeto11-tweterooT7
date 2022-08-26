43a87d74019faf715d45518043fff0f7979159f2
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-08-26 10:45:32 -0300

    Implementa o post sign-up

--- src/app.js
+++ src/app.js
@@ -4,13 +4,25 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 const porta = 5000;
 
 app.get("/", (req, res) => {
     console.log("Olá, mundo!")
     res.send("Olá, mundo!");
-})
+});
 
-app.listen(5000,()=>{
+app.get("/:nome",(req,res)=>{
+    const nome = req.params.nome;
+    res.send(`Seu parâmetro é ${nome}`);
+});
+
+const usuarios = [];
+app.post("/sign-up",(req,res)=>{
+    usuarios.push(req.body);
+    res.send("OK");
+});
+
+app.listen(porta,()=>{
     console.log(`Servidor aberto na porta ${chalk.blue(porta)}`)
 });
