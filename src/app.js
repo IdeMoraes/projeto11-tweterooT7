d36e53bdca487926d9d29b11a7ef52b38971d5fd
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-08-29 16:15:34 -0300

    Implementa o get tweets

--- src/app.js
+++ src/app.js
@@ -7,16 +7,6 @@ app.use(cors());
 app.use(express.json());
 const porta = 5000;
 
-app.get("/", (req, res) => {
-    console.log("Olá, mundo!")
-    res.send("Olá, mundo!");
-});
-
-app.get("/:nome",(req,res)=>{
-    const nome = req.params.nome;
-    res.send(`Seu parâmetro é ${nome}`);
-});
-
 const usuarios = [];
 app.post("/sign-up",(req,res)=>{
     usuarios.push(req.body);
@@ -27,6 +17,23 @@ const tweets = [];
 app.post("/tweets",(req,res)=>{
     tweets.push(req.body);
     res.send("OK");
+});
+
+app.get("/tweets", (req, res) => {
+    let ultimosTweets = [];
+    if(tweets.length<10){
+        ultimosTweets = [...tweets];
+    }
+    else {
+        for(let i=tweets.length-10; i<tweets.length; i++){
+            ultimosTweets.push(tweets[i]);
+        };
+    }
+
+    for(let i =0; i<ultimosTweets.length; i++){
+        ultimosTweets[i].avatar = usuarios.find(element => element.username === ultimosTweets[i].username).avatar;
+    }
+    res.send(ultimosTweets);
 });
 
 app.listen(porta,()=>{
